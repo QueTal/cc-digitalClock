8cb2d4afce6cab3cdff5b85100332110cd6d1e43
quetal <gibum1995@gmail.com>
2021-01-15 00:04:11 +0900

    day color error remove

--- Day.js
+++ Day.js
@@ -4,8 +4,11 @@ const week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 function showDay(){
     const date = new Date();
     const day = date.getDay();
-    const x = document.getElementById(day);
-    x.style.color = "red";
+    const cur = document.getElementById(day);
+    const prev = document.getElementById(day == 0? 6 : day-1);
+
+    prev.style.color = "white";
+    cur.style.color = "red";
     setTimeout(showDay, 1000);
 }
 showDay();
